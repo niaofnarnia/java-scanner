5b216a4d6ba6e7b14bc71494c9b7fca1bb94c07e
Nia <niaespinal@outlook.com>
2025-05-05 15:00:15 +0200

    Completed SignUp exercise

--- SignUp.java
+++ SignUp.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class SignUp {
     public static void main(String[] args) {
-        //Vamos a crear un formulario de registro
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("\nIntroduce tu nombre:");
+        String nombre = scanner.nextLine();
+
+        System.out.println("\nIntroduce tu apellido:");
+        String apellido = scanner.nextLine();
+
+        System.out.println("\nElige un nombre de usuario:");
+        String usuario = scanner.nextLine();
+
+        System.out.println("\nElige una contraseña:");
+        String contrasena = scanner.nextLine();
+
+        System.out.println("\nHola " + nombre + " " + apellido + ", tu nombre de usuario es " + usuario + " y tu contraseña es " + contrasena + ", gracias por registrarte.");
+    }
+}
+        //Vamos a crear un formulario de registro
         //Los datos que debes pedir al usuario son:
         //1. Nombre
         //2. Apellido
@@ -13,5 +31,3 @@ public class SignUp {
         //Imprimir el siguiente resultado:
         //Hola <nombre> <apellido>, tu nombre de usuario es <usuario> y tu contraseña es <contraseña>, gracias por registrarte.
         //Añade una nueva línea antes de mostrar la respuesta
-    }
-}
